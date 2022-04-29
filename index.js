d81fd34a12e5c8e0815fbd20bbc8090e3cfcfcd6
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-04-29 11:04:24 +0300

    Add declaration & initialization variable

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-console.log(12);
-console.log('Elon Musk');
-console.log(true);
-console.log(false);
-console.log(undefined);
-console.log(null);
+//variable
+/*
+  имя переменной
+  начинается только с буквы или _ или $
+  латиница, без пробелов, цифры не первые
+  регистрозависимая 
+  camelCase
+  отвечает на вопрос Что в ней
+*/
+let num1; //объявление
+console.log(num1);
+// console.log(num2);
+let num2 = 22;
+console.log(num2);
+num1 = 11; //инициализация
+num1 = 66;
+console.log(num1);
+const num3 = 33;
