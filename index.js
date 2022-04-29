c119ea2b4268859535aee3cd1c794006eeb19b77
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-04-29 12:16:23 +0300

    Add Type Conversion to Boolean

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-let string1 = 'I\'m a teacher';
-console.log(string1,':', typeof string1);
+let condition = 'false'-3;
+console.log("value = ", condition, typeof condition);
 
-let age = '32';
+if (condition) {
+  console.log("value = ", condition, typeof condition);
+  //только если в кругліх скобках выражение правдиво
+}
 
-let price = '   5   ';
-
-
-let num = 8;
-console.log(num,':', typeof num);
-
-let undef = undefined;
-console.log(undef,':', typeof undef);
-
-let nullValue = null;
-console.log(nullValue,':', typeof nullValue);
+/*
+false
+undefined
+null
+NaN
+'' //empty string
+0  +0  -0
+*/
