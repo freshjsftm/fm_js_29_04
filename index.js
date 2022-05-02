76737548a223b6f20b1a45a4c300a12e048d26b0
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-02 11:33:25 +0300

    Example && , ||

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-// < <= > >=
-console.log(10>10);
-console.log(10>=10);
+//Logical AND (&&) умножение
+//true&&true=true     1*1=1
+//false&&true=false   0*1=0
+//true&&false=false   1*0=0
+//false&&false=false  0*0=0
 
-console.log('Abd '<='Abd');
+//Logical OR (||) сложение
+//true||true=true     1+1=2
+//false||true=true    0+1=1
+//true||false=true    1+0=1
+//false||false=false  0+0=0
 
-console.log(true > false);
+let logic = 10 && "false" && 45;
+console.log(logic);
+
+logic = 0 || false || null;
+console.log(logic);
