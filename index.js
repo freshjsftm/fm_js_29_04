c1d9e70b1868cd7cf58209d4dd1a62210befc823
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-02 10:58:45 +0300

    Example Logical NOT

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-//Increment
-let number_1 = 2;
-// console.log(number_1);
-// number_1++; //	Postfix Increment
-// ++number_1; //	Prefix Increment
+//Logical NOT (!)
+//неявно приводит к логическому типу 
+//и потом выполняет инверсию
+//в результате выполнения будет только логический тип
 
-// console.log(number_1++);
+//ложь это:
+//false
+//null
+//undefined
+//NaN
+//0  +0  -0
+//'' - empty string
 
-// console.log(number_1);
-
-let result = number_1++ * 10;
-console.log(number_1);
+let logic = 'Elon';//true
+let result = !logic;
+console.log(!logic);//!true -> false
+console.log(logic);
 console.log(result);
-
-
