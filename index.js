db8958fc6456edde23e45cbedcfe0f18b93f1898
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-03 09:38:16 +0300

    Example prompt

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-//Assignment
 
-let age = 10;
-//age = age + 5*4/2;
-age += 5*4/2;
 
-let num = 3;
-//num = num * num;
-num *= num;
+let str = prompt('Message','default');
 
-let num2 = 56;
-num2 /= 2;
+console.log(str);
 
-let price = 10;
-price = (price+5)*1.03;
+
+
+
 
 
 
