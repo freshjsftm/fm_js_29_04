c0880d688cbaf717d640d2d97224e3716c352f76
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-03 10:49:05 +0300

    Refactor first calcSummaTwoNamberReqAtUser

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const calcSummaTwoNamberReqAtUser = function(){
   let number1 = prompt('Enter number 1', '10');
   let number2 = prompt('Enter number 2', '20');
 //ошибка: если пустая строка либо нажали отмена либо ввели что-то, что не цифры
-  if(number1==='' || number2==='' || number1===null ||number2===null || isNaN(Number(number1)) || isNaN(Number(number2))){
+  const error = number1==='' || number2==='' || number1===null || number2===null || isNaN(Number(number1)) || isNaN(Number(number2));
+  if(error){
     //true
     //обработка ошибки
-    return 'error';
-  }else{
-    //false
-    let summa = Number(number1) + Number(number2);
-    return summa;
-  } 
+    return 'error'; //exit
+  }
+  //let summa = Number(number1) + Number(number2);
+  //return summa;
+  return Number(number1) + Number(number2);
+ 
 }
 
 let summa = calcSummaTwoNamberReqAtUser();
