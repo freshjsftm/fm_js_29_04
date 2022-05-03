b9c74bbaab61ec01be26c66a1d558a296dd416fe
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-03 10:26:46 +0300

    Add function calcSummaTwoNamberReqAtUser

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-// function requestNumberAtUser(){
-//   let number = prompt('enter number', '10');
-// }
-
-//function expression
-const requestNumberAtUser = function(){
-  let number = prompt('enter number', '10');
+const calcSummaTwoNamberReqAtUser = function(){
+  let number1 = prompt('Enter number 1', '10');
+  let number2 = prompt('Enter number 2', '20');
+//ошибка: если пустая строка либо нажали отмена либо ввели что-то, что не цифры
+  if(number1==='' || number2==='' || number1===null ||number2===null || isNaN(Number(number1)) || isNaN(Number(number2))){
+    //true
+    //обработка ошибки
+    return 'error';
+  }else{
+    //false
+    let summa = Number(number1) + Number(number2);
+    return summa;
+  } 
 }
 
-console.log(requestNumberAtUser);
+let summa = calcSummaTwoNamberReqAtUser();
+console.log(summa);
+
+
+
 
 
 
