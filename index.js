027e1cf1a62fcd0f7c20892136e66cd506755937
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-04 10:54:06 +0300

    Add isAliquotAndBigger

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-//age>=18
-/**
- * 
- * @param {number} age 
- * @returns {boolean}
- */
-const isAdult = function(age=0, ADULT=18){
-  return age>=ADULT;
+// const isDivide = function(num1=20, num2=7) {
+//   return num1 % num2 === 0;
+// }
 
-  // if(age<18){
-  //   return false;
-  // }
-  // return true;
-}
-const result = isAdult(18, 21);
-console.log(result);
+// const calccrat = function(num1=0,num2=7,num3=20){
+//   let check = num1>num3 && num1%num2===0; //true
+//   if(check){
+//     return true;
+//   }else{
+//     return false;
+//   };
+// };
+
+// const isAliquotAndBigger = function (number = 0, aliquot = 7, bigger = 20) {
+//   return number > bigger && number % aliquot === 0;
+// };
+
+// const b = 'qwe';
+
+// const isMoreThan20andMultipleTo7 = function (c) {
+//   // let b = 7;
+//   // let a = 20;   
+//   return c % 7 === 0 && c > 20;
+// }
+
+// console.log(isMoreThan20andMultipleTo7(21));
+// console.log(b);
