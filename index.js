c30df58fd44d9881028b2213cda469b73e5a882e
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-04 09:46:12 +0300

    Add calcPerimetrSquare

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
-//const number1 = prompt("Enter number 1", "10");
-//const number2 = prompt("Enter number 2", "20");
+//найти периметр квадрат
+//сторона*4
+
 /**
- * Description function
- * @param {null|string} num
- * @returns {boolean}
+ * 
+ * @param {number} side 
+ * @returns {null | number}
  */
-const isErrorInputNumber = function (num) {
-  return num === "" || num === null || isNaN(Number(num));
-};
-/**
- * Return summa two number.
- * @param {number} number1 default = 1
- * @param {number} number2 default = 2
- * @returns {number}
- */
-const calcSummaTwoNumber = function (number1=1, number2=2) {
-  return number1 + number2;
-};
-//debugger
-
-console.log(calcSummaTwoNumber(undefined,45));
-
-// const check = isErrorInputNumber(number1) || isErrorInputNumber(number2);
-// if (check===false) {  //!check - bad practic
-//   const summaUser = calcSummaTwoNumber(Number(number1),Number(number2));
-//   console.log(summaUser);
-// } else {
-//   console.log('not possible calc summa');
-// }
-
-
+const calcPerimetrSquare = function(side=0){
+  if(side<0){
+    return null;
+  }
+  return side*4;
+}
+const result = calcPerimetrSquare(8);
+console.log(result);
