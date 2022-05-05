50e03ff61bbf4a879ef76fede2e59d4a2683030a
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-05 11:15:58 +0300

    Example switch

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-const userNumber = "22";
-debugger //+prompt('Enter number');
-if (isNaN(userNumber)) {
-  console.log(userNumber, "NAN");
-} else if(userNumber % 5 === 0) {
-  console.log(userNumber, "multiply 5");
-} else if(userNumber % 3 === 0){
-  console.log(userNumber, "multiply 3");
-} else if(userNumber % 2 === 0){
-  console.log(userNumber, "multiply 2");
-} else {
-  console.log(userNumber, "multiply 1");
+const lang = prompt("Choose lang:\n1-ua\n2-it\n3-en\n4-es");
+//debugger;
+switch (lang) {
+  case "1":
+  case "ua":
+  case "ukraine":
+    console.log("привіт");
+    break;
+  case "2":
+  case "it":
+    console.log("chao");
+    break;
+  case "3":
+  case "en":
+    console.log("hi");
+    break;
+  case "4":
+  case "es":
+    console.log("hola");
+    break;
+  default:
+    console.log("repeat choose");
+    break;
 }
+console.log("end");
 
-console.log("thanks");
-// if (isNaN(userNumber)) {
-//   console.log("NAN");
-// } else {
-//   if (userNumber % 5 === 0) {
-//     console.log("multiply 5");
-//   } else {
-//     if (userNumber % 3 === 0) {
-//       console.log("multiply 3");
-//     } else {
-//       if (userNumber % 2 === 0) {
-//         console.log("multiply 2");
-//       }
-//     }
-//   }
+// if(lang==='1' || lang==='ua'){
+//   console.log('привіт')
+// }else if(lang==='2' || lang==='it'){
+//   console.log('chao')
+// }else if(lang==='3' || lang==='en'){
+//   console.log('hi')
+// }else if(lang==='4' || lang==='es'){
+//   console.log('hola')
+// }else{
+//   console.log('repeat choose')
 // }
-
-
+// console.log('end')
