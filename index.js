6341c7ed5cec8feda2bfb642f59581e6bae47366
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-05 09:41:51 +0300

    Example ternary operator

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-// const isDivide = function(num1=20, num2=7) {
-//   return num1 % num2 === 0;
-// }
+//Conditional (ternary) operator
 
-// const calccrat = function(num1=0,num2=7,num3=20){
-//   let check = num1>num3 && num1%num2===0; //true
-//   if(check){
-//     return true;
-//   }else{
-//     return false;
-//   };
-// };
+//true|false
+const isEven = function(number=0){
+  return number%2 === 0;
+}
 
-// const isAliquotAndBigger = function (number = 0, aliquot = 7, bigger = 20) {
-//   return number > bigger && number % aliquot === 0;
-// };
-
-// const b = 'qwe';
-
-// const isMoreThan20andMultipleTo7 = function (c) {
-//   // let b = 7;
-//   // let a = 20;   
-//   return c % 7 === 0 && c > 20;
-// }
-
-// console.log(isMoreThan20andMultipleTo7(21));
-// console.log(b);
+//'even'|'odd'
+const returnEvenOrOdd = function(number=0){
+  return (number%2 === 0) ? 'even' : 'odd';
+}
