5656b7a92bb2a9197ecd101c671b2217566493c7
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-05 10:09:36 +0300

    Used Conditional (ternary) operator

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 //Conditional (ternary) operator
 
-//true|false
-const isEven = function(number=0){
-  return number%2 === 0;
-}
+// //true|false
+// const isEven = function(number=0){
+//   return number%2 === 0;
+// }
 
-//'even'|'odd'
-const returnEvenOrOdd = function(number=0){
-  return (number%2 === 0) ? 'even' : 'odd';
-}
+// //'even'|'odd'
+// const returnEvenOrOdd = function(number=0){
+//   return number%2 === 0 ? 'even' : 'odd';
+// }
+
+// const returnAdalt = function(number=0){
+//   return (number>=18) ? 'adalt' : 'not adalt';
+// }
+
+// const checkAges = function(number = 20) {
+//   (number >= 18) ? 'adult' : 'not adult';
+// }
+
+// console.log(checkAges(prompt()));
+
+const summa = 123456;
+const LIMIT_SUMMA = 10000;
+
+const sale = summa > LIMIT_SUMMA ? 0.1 : 0.02;
+
+//const classInput = isValid ? 'valid' : 'invalid';
+
+
