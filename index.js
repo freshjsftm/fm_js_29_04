6e5456d130297e1d7495d863bf002063dff93251
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-05 12:07:44 +0300

    Example variantes solve

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-const age = '18';//prompt()
-//[0-3) - baby
-//[3-6) - tudler
-//[6-13) - children
-//[13-18) - tinager
-//[18-70) - adult
-//[70-100) - oldmen
-//[100-Infinity) - respect
-debugger
-if(isNaN(age) || age<0){
-  console.log('error')
-}else if(age<3){
-  console.log('baby')
-}else if(age<6){
-  console.log('tudler')
-}else if(age<13){
-  console.log('children')
-}else if(age<18){
-  console.log('tinager')
-}else if(age<70){
-  console.log('adult')
-}else if(age<100){
-  console.log('oldmen')
+// const season = '15';//"\n-winter\n-spring\nsummer\n-fall";
+
+// switch (season) {
+//   case "1":
+//   case "2":
+//   case "12":
+//     console.log("winter");
+//     break;
+//   case "3":
+//   case "4":
+//   case "5":
+//     console.log("spring");
+//     break;
+//   case "6":
+//   case "7":
+//   case "8":
+//     console.log("summer");
+//     break;
+//   case "9":
+//   case "10":
+//   case "11":
+//     console.log("fall");
+//     break;
+//   default:
+//     console.log("fail");
+//     break;
+// }
+
+
+const month = prompt("enter month number", 1);
+//debugger;
+if (isNaN(month) || month <= 0) {
+  console.log("error");
+} else if (month <= 2) {
+  console.log("winter");
+} else if (month <= 5) {
+  console.log("spring");
+} else if (month <= 8) {
+  console.log("summer");
+} else if (month <= 11) {
+  console.log("fall");
+}else if (month <= 12) {
+  console.log("winter");
 }else{
-  console.log('respect')
+  console.log("fail");
 }
