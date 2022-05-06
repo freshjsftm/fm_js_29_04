4d7b5ca6f699042e5b729e8dfb4111d21bd53335
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-06 11:20:49 +0300

    Example password while(countTry < MAX_TRY)

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 let countTry = 0;
-while(true){
+while(countTry < MAX_TRY){
   const userInput = prompt('enter password','***');
   countTry++;
   alert('try = ' + countTry);
-  if(countTry >= MAX_TRY){
-    alert('try end');
-    break;
-  }
   if(userInput === CORRECT_PASSWORD){
     alert('welcome');
     break;
