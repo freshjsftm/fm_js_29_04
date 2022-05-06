f30560825b8c921e74224a34ba94444d5dcd994d
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-06 10:31:36 +0300

    Example while

--- index.js
+++ index.js
@@ -1,56 +1,16 @@
-const sum = function (num1, num2) {
-  return num1 + num2;
-};
-const sub = function (num1, num2) {
-  return num1 - num2;
-};
-const mult = function (num1, num2) {
-  return num1 * num2;
-};
-const div = function (num1, num2) {
-  return num1 / num2;
-};
-const rem = function (num1, num2) {
-  return num1 % num2;
-};
+// let count = 2;
+// //debugger
+// while(count>0){
+//   console.log(count);
+//   count--;
+// }
 
-const calculate = function (num1 = 10, num2 = 2, operator = "+") {
-  if (isNaN(num1 * num2)) {
-    return false;
-  }
-  let f = null;
-  switch (operator) {
-    case "+":
-      f = sum;
-      break;
-    case "-":
-      f = sub;
-      break;
-    case "*":
-      f = mult;
-      break;
-    case "/":
-      f = div;
-      break;
-    case "%":
-      f = rem;
-      break;
-    default:
-      return null;
-  }
-  if (typeof f === "function") {
-    return f(num1, num2);
-  }
-  return false;
-};
 //debugger
-console.log(typeof calculate);
-console.log(calculate(undefined, undefined, "-"));
-console.log(calculate(4,5,'*'));
-
-const hightOrderFunction = function(num1,num2,func){
-  return func(num1,num2);
+let count = 0;
+while (true) {
+  count++;
+  if (count > 3){
+    break;
+  } 
+  console.log(count);
 }
-console.log(hightOrderFunction(4,3,mult));
-console.log(hightOrderFunction(74,3,div));
-
