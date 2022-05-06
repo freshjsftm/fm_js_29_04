67add1d305f0182da98a50f5e3e7b405a7c6e6d8
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-06 09:56:28 +0300

    Example clearing  function & hightOrderFunction

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
-// const season = '15';//"\n-winter\n-spring\nsummer\n-fall";
+const sum = function (num1, num2) {
+  return num1 + num2;
+};
+const sub = function (num1, num2) {
+  return num1 - num2;
+};
+const mult = function (num1, num2) {
+  return num1 * num2;
+};
+const div = function (num1, num2) {
+  return num1 / num2;
+};
+const rem = function (num1, num2) {
+  return num1 % num2;
+};
 
-// switch (season) {
-//   case "1":
-//   case "2":
-//   case "12":
-//     console.log("winter");
-//     break;
-//   case "3":
-//   case "4":
-//   case "5":
-//     console.log("spring");
-//     break;
-//   case "6":
-//   case "7":
-//   case "8":
-//     console.log("summer");
-//     break;
-//   case "9":
-//   case "10":
-//   case "11":
-//     console.log("fall");
-//     break;
-//   default:
-//     console.log("fail");
-//     break;
-// }
+const calculate = function (num1 = 10, num2 = 2, operator = "+") {
+  if (isNaN(num1 * num2)) {
+    return false;
+  }
+  let f = null;
+  switch (operator) {
+    case "+":
+      f = sum;
+      break;
+    case "-":
+      f = sub;
+      break;
+    case "*":
+      f = mult;
+      break;
+    case "/":
+      f = div;
+      break;
+    case "%":
+      f = rem;
+      break;
+    default:
+      return null;
+  }
+  if (typeof f === "function") {
+    return f(num1, num2);
+  }
+  return false;
+};
+//debugger
+console.log(typeof calculate);
+console.log(calculate(undefined, undefined, "-"));
+console.log(calculate(4,5,'*'));
 
-
-const month = prompt("enter month number", 1);
-//debugger;
-if (isNaN(month) || month <= 0) {
-  console.log("error");
-} else if (month <= 2) {
-  console.log("winter");
-} else if (month <= 5) {
-  console.log("spring");
-} else if (month <= 8) {
-  console.log("summer");
-} else if (month <= 11) {
-  console.log("fall");
-}else if (month <= 12) {
-  console.log("winter");
-}else{
-  console.log("fail");
+const hightOrderFunction = function(num1,num2,func){
+  return func(num1,num2);
 }
+console.log(hightOrderFunction(4,3,mult));
+console.log(hightOrderFunction(74,3,div));
+
