5f70a110f2fe8d6c84156f9a97d895418ddb0282
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-06 11:49:10 +0300

    Example for loop

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 const CORRECT_PASSWORD = 'qwe123';
-const MAX_TRY = 3;
+const MAX_TRY = 2;
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let countTry = 0;
-while(countTry < MAX_TRY){
-  const userInput = prompt('enter password','***');
-  countTry++;
-  alert('try = ' + countTry);
-  if(userInput === CORRECT_PASSWORD){
-    alert('welcome');
+debugger
+// let countTry = 0; //i
+// while(countTry < MAX_TRY){ //i<MAX_TRY
+//   const userInput = prompt('enter password','***');
+//   countTry++; //i++
+//   alert('try = ' + countTry);
+//   if(userInput === CORRECT_PASSWORD){
+//     alert('welcome');
+//     break;
+//   }
+// }
+
+for (let i = 1; i <= MAX_TRY; i++) {
+  const userInput = prompt("enter password", "***");
+  alert("try = " + i);
+  if (userInput === CORRECT_PASSWORD) {
+    alert("welcome");
     break;
   }
 }
