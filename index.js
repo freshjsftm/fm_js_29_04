f28b49a14ee15fd5d1ef62b9bb74b69ec849cf49
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-09 11:12:44 +0300

    Example constructor function

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 'use strict'; //ES2015
-const cat1 = {   //literal
-  name:'Muha',
-  isMale:false,
-  age:3,
-  breed: 'street',
-  color: 'grey',
-  say:function(){
-    return 'meow';
-  },
-  run:function(){
-    return 'i\'m runing';
-  },
-};
-console.log(cat1);
-console.log(cat1.age)
-cat1.age++;
-console.log(cat1.say());
-console.log(cat1.run())
-cat1.colorEye = 'green';
-console.log(cat1);
-//cat1.colorEye = undefined;
-delete cat1.breed;
-console.log(cat1);
+
+
+function Dog(name='Test',isMale=true){
+  this.name = name;
+  this.isMale = isMale;
+  this.say = function(voice='Wof'){
+    return voice;
+  }
+}
+
+const testDog = new Dog();
+console.log(testDog);
+const dog1 = new Dog("Oscar",true);
+console.log(dog1);
+console.log(dog1.say());
+
+
