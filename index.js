1e9bdb85c3160c4e07c39d05323ff8d2a791075e
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-11 09:26:06 +0300

    Example copy by link

--- index.js
+++ index.js
@@ -1,42 +1,13 @@
 'use strict';
 
-const obj = {
-  prop1:12,
-  'prop 2':true,
-  11:44,
-};
+const pr1 = 12;
+const pr2 = 12;
 
-// const key = 'prop 2';
-// console.log(obj);
-// console.log(obj["prop1"]);
-// console.log(obj["prop 2"]);
-// console.log(obj["11"]);
-// console.log(obj[key]); //obj["prop 2"] true
+console.log(pr1===pr2);
 
-// const valueKey = 'new prop';
-// obj[valueKey] = 12;
-
-
-
-
-// const table = {
-//   '2*2':4,
-//   '2*3':6,
-// }
-
-//вернуть объект таблица умножения
-const createMultTable = function(limit=4){
-  const table = {};
-  for(let i=2; i<=limit; i++){
-    for(let j=2; j<=limit; j++){
-      table[`${i}*${j}`]=i*j;
-    }
-  }
-  return table;
-}
-//debugger
-const table = createMultTable();
-console.log(table)
-
-const str = '2*3';
-console.log(table[str]);
+const ob1 = {};
+const ob2 = {};
+const newOb = ob1;//копирование по ссылке
+console.log(ob1===ob2);//false
+console.log(ob1===newOb);//true
+newOb.prop = 12;
