13207402383913d07eb67305cf74e4f0ffdcdac4
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-11 10:48:48 +0300

    Create MyArray version 1.0

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 'use strict';
 
-const object1 = {};
-const object2 = new Object();
-const object3 = Object();
-
-console.log(object1);
-console.log(object2);
-console.log(object3);
-
-function MyObject(){
-  if(!new.target){
-    return new MyObject();
+function MyArray(){
+  this.length=0;
+  this.push = function(value){
+    this[this.length] = value;
+    this.length++;
+    return this.length;
   }
 }
 
-const obj1 = MyObject();// create object without new
-console.log(obj1);
+const myArray1 = new MyArray();
+const myArray2 = new MyArray();
+console.log(myArray1.length);
+console.log(myArray1.push(8));
+console.log(myArray1.push(7));
+console.log(myArray1);
+console.log(myArray1.push===myArray2.push);
+
+const array1 = new Array();
+const array2 = new Array();
+console.log(array1.length);
+console.log(array1.push(8));
+console.log(array1===array2);
+console.log(array1.push===array2.push);//
