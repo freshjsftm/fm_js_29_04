165925ad7140be2d2dfa6f67e1f2a7e300a8ebbb
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-11 09:15:01 +0300

    Example [] Computed Member Access

--- index.js
+++ index.js
@@ -1,77 +1,42 @@
-"use strict";
-// let n = 10;
-// const str1 = 'dsfd\''+n+'sfs';
-// const str2 = `let n = ${n}
-// trenningMath('www',10) = ${trenningMath('www',10)}`;
-// alert(str2);
+'use strict';
 
-const logRangeNumber = function (max = 10, min = 1, number = 3) {
-  //проверка на вход
-  if (max < min) {
-    return false;
-  }
-  //выводить числа из диапозона в обратном порядке
-  let i = max;
-  while (i >= min) {
-    //логировать НЕ кратные числу
-    if (i % number === 0) {
-      i--;
-      continue;
-    }
-    console.log(i);
-    i--;
-  }
-  return true;
+const obj = {
+  prop1:12,
+  'prop 2':true,
+  11:44,
 };
 
-/*
-2) предлагать пользователю решить арифметический пример до тех пор, пока он его не решит.
-+ создать функцию продумать параметры
-+ используя параметры, сформировать промпт
-+ вычислить пример, который получился из параметров
-- сравнить ваши вычисления и ответ пользователя
-*/
-/**
- *
- * @param {number} number1
- * @param {number} number2
- * @param {string} operator *|-|+|/
- * @returns {boolean}
- */
-const trenningMath = function (number1 = 10, number2 = 2, operator = "+") {
-  //выполнить рефакторинг кода - вынести расчет в функцию
-  //const result = calc(number1, number2, operator);
-  //if(result===null){return false;}
-  if (isNaN(number1 * number2)) {
-    return false;
-  }
-  let result = null; 
-  switch (operator) {
-    case "*":
-      result = number1 * number2;
-      break;
-    case "-":
-      result = number1 - number2;
-      break;
-    case "/":
-      result = number1 / number2;
-      break;
-    case "+":
-      result = number1 + number2;
-      break;
-    default:
-      return false;
-  }
-  while(true){
-    const answerUser = prompt(
-      `${number1} ${operator} ${number2} = `,
-      "your answer"
-    );
-    if(result === Number(answerUser)){
-      return true;
+// const key = 'prop 2';
+// console.log(obj);
+// console.log(obj["prop1"]);
+// console.log(obj["prop 2"]);
+// console.log(obj["11"]);
+// console.log(obj[key]); //obj["prop 2"] true
+
+// const valueKey = 'new prop';
+// obj[valueKey] = 12;
+
+
+
+
+// const table = {
+//   '2*2':4,
+//   '2*3':6,
+// }
+
+//вернуть объект таблица умножения
+const createMultTable = function(limit=4){
+  const table = {};
+  for(let i=2; i<=limit; i++){
+    for(let j=2; j<=limit; j++){
+      table[`${i}*${j}`]=i*j;
     }
   }
-};
+  return table;
+}
+//debugger
+const table = createMultTable();
+console.log(table)
 
-const message = trenningMath() ? 'win' : 'error';
-alert(message);
+const str = '2*3';
+console.log(table[str]);
