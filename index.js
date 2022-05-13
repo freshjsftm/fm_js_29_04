489c7af36989973d2257fe66d3d5d36b20353dad
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-13 09:19:10 +0300

    Example HOF

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-"use strict";
-function StudentPrototypePrototype(){
-  this.eat=function(){
-    return this.name + ' is eating';
-  };  
+/**
+ * 
+ * @param {function} howSay 
+ * @param {string} word 
+ */
+function sayWord(howSay,word='Qwerty'){
+  howSay(word);
 }
-function StudentPrototype(){
-  this.toString = function() {
-    return `${this.name} ${this.sname}`;
-  };
-  this.read=function(){
-    return this.name + ' is reading';
-  }
-}
-StudentPrototype.prototype = new StudentPrototypePrototype();
 
-function Student(name, sname, age=22){
-  if(!new.target){
-    return new Student(name, sname, age);
-  }  
-  this.name = name;
-  this.sname = sname; 
-  this.age = age;
-};
-Student.prototype = new StudentPrototype();
-
-const student = new Student("Elon","Musk", 50);
-console.log(student);
-console.log(student.toString());
-console.log(student.read());
-console.log(student.eat());
+sayWord(console.log, 'asd');
+// sayWord(alert,'dog');
+// sayWord(prompt,'dog');
+// sayWord(confirm,'dog');
