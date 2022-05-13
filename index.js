c7e32e53dfa51afb4bd910deaa58e750bda44fcd
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-13 10:39:09 +0300

    Add filter, some,every

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-/**
- * 
- * @param {function} howSay 
- * @param {string} word 
- */
-function sayWord(howSay,word='Qwerty'){
-  howSay(word);
-}
+'use strict';
 
-sayWord(console.log, 'asd');
-// sayWord(alert,'dog');
-// sayWord(prompt,'dog');
-// sayWord(confirm,'dog');
+const arr = [1,2,3,4,5];
+const isEven = function(elem){
+  return elem%2 === 0;
+}
+const newArr = arr.filter(isEven);
+console.log(newArr);
+
+
+// function sayWord(howSay, whatSay){}
+
+// function log(n){
+//   console.log(n);
+//   return;//undefined
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//   log(arr[i]); 
+// }
+// //debugger
+// arr.forEach(log);
+
+// arr.forEach(
+//   function(element){
+//     return element+5;
+//   }
+// )
+
+// const users = [{},{},{}];
+// users.forEach(function(user){
+//   user.stars = 1;
+// });
+// console.log(users);
--- js/MyArray.js
+++ js/MyArray.js
@@ -19,8 +19,33 @@ function MyArrayProptotype() {
   };
   this.forEach = function(callback){
     for (let i = 0; i < this.length; i++) {
-      callback(this[i]);
+      callback(this[i],i,this);
     }
+  }
+  this.filter = function(callback){
+    const newMyArray = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+      if(callback(this[i],i,this)){
+        newMyArray.push(this[i]);
+      } 
+    }
+    return newMyArray;
+  }
+  this.some = function(callback){
+    for (let i = 0; i < this.length; i++) {
+      if(callback(this[i],i,this)){
+        return true;
+      } 
+    }
+    return false;
+  }
+  this.every = function(callback){
+    for (let i = 0; i < this.length; i++) {
+      if(callback(this[i],i,this)===false){
+        return false;
+      } 
+    }
+    return true;
   }
 }
 //конструктор объекта с данными
@@ -38,4 +63,8 @@ function log(n){
 MyArray.prototype = new MyArrayProptotype();
 
 const myArray = new MyArray(1, 2, 3, 8, 5);//5
-myArray.forEach(log);
+//myArray.forEach(log);
+const newArray = myArray.filter(function(elem){
+  return elem>=5;
+});
+console.log(newArray);
