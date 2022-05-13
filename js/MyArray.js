39dcf1ed89a20833267992046a906f6bd28cfc7c
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-13 10:05:22 +0300

    Add forEach MyArray

--- js/MyArray.js
+++ js/MyArray.js
@@ -3,9 +3,6 @@
 //контсруктор прототипа-родителя с логикой с методами
 function MyArrayProptotype() {
   this.push = function () {
-    //this[this.length] = value;
-    //this.length++;
-    //return this.length;
     for (let i = 0; i < arguments.length; i++) {
       this[this.length] = arguments[i];
       this.length++;
@@ -18,9 +15,13 @@ function MyArrayProptotype() {
     }
     const lastItem = this[this.length - 1];
     delete this[--this.length];
-    //this.length;
     return lastItem;
   };
+  this.forEach = function(callback){
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i]);
+    }
+  }
 }
 //конструктор объекта с данными
 function MyArray() {
@@ -30,28 +31,11 @@ function MyArray() {
   }
 }
 
+function log(n){
+  console.log(n*n);
+}
+
 MyArray.prototype = new MyArrayProptotype();
-MyArray.prototype.qqq = function () {
-  console.log("newParent");
-};
 
-const myArray1 = new MyArray(1, 2, 3, 8, 5);
-myArray1.qqq();
-// console.log(myArray1.prototype);
-const myArray2 = new MyArray();
-console.log(myArray1.length);
-//console.log(myArray1.push(8,5,9));
-console.log(myArray1);
-console.log(myArray1.push === myArray2.push);
-
-const array1 = new Array(1, 2, 3, 5);
-// const array2 = new Array();
-console.log(array1);
-// console.log(array1.push(8));
-// console.log(array1 === array2);
-// console.log(array1.push === array2.push);
-
-Array.prototype.qqq = function () {
-  console.log("newParent");
-};
-array1.qqq();
+const myArray = new MyArray(1, 2, 3, 8, 5);//5
+myArray.forEach(log);
