18ef83b4d978863a036aabf29600b60f550d9020
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-17 09:52:00 +0300

    Example new Boolean

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 "use strict";
 
-/*
-напишите функцию стрелку, которая принимает два числа и возвращает большее из двух
-n1>n2 ? n1 : n2
-*/
-//const getMaxFromTwoNumber = (n1, n2)=>{return n1>n2 ? n1 : n2; }
-const getMaxFromTwoNumber = (n1, n2)=> n1>n2 ? n1 : n2; 
+const check  = new Boolean(false);
+console.dir(check);//object
 
-const showArgs = function(a,b, ...c) {
-  console.log(c)
-  c.forEach((item)=>{
-    console.log(this)
-  })
-}
-showArgs(1,2,3,4,5);///undefined
+//0 undefined '' NaN null false
+const str = '';
 
-const obj = {
-  prop:12,
-  showArgs,
+if(Boolean(str)){
+  console.log('yes')
+}else{
+  console.log('no') 
 }
-obj.showArgs(1,1,1,1,1);///obj
