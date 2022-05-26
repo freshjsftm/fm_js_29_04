f6a563fe00e51d2ed0a3fd08e79146825cfec728
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-26 12:20:14 +0300

    Add checkSequence

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 "use strict";
+
+// const checkSequence = (str) => {
+//   let amount1 = 0;
+//   let amount2 = 0;
+//   for (const symbol of str) {
+//     if(symbol==='('){
+//       amount1++
+//     }
+//     if(symbol===')'){
+//       amount2++
+//     }
+//   }
+//   return amount1===amount2;
+// }
+
+// const checkSequence = (str) => { 
+//   const stack = new Stack();
+//   for (const symbol of str) {
+//     if(symbol==='('){ 
+//       stack.push(symbol);
+//     }
+//     if(stack.isEmpty){ 
+//       return false;
+//     }
+//     if(symbol===')'){
+//       stack.pop();
+//     }
+//   }
+//   return stack.isEmpty;
+// }
+
+// console.log(checkSequence('()()'));
+
+
+const options = {
+  brackets:{
+    '(':')',
+    '{':'}',
+    '[':']',
+  }
+}
+
+const checkSequence = (str, options) => { 
+  const brackets = options.brackets; //{'(':')','{':'}','[':']',}
+  //тут надо собрать все значения объекта brackets в массив
+  const stack = new Stack();
+  for (const symbol of str) {  //')'
+    if(brackets[symbol] !== undefined){ //brackets['(']=>')'
+      stack.push(symbol);               //'('
+      continue;
+    }
+    // if(stack.isEmpty){ 
+    //   return false;
+    // }
+    const lastOfStack = stack.pick(); //'('
+    const correctSymbol = brackets[lastOfStack];
+    if(symbol === correctSymbol){ //')'
+      stack.pop();
+    }else{ //тут надо дать возможность пропускать не скобки
+      return false;
+    }
+  }
+  return stack.isEmpty;
+}
+console.log(checkSequence(')()', options));
