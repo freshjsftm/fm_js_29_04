7e8201117ee16efa9c6404e2d7600f194ace2517
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-27 09:22:55 +0300

    Refactor checkSequence

--- index.js
+++ index.js
@@ -17,13 +17,13 @@
 // const checkSequence = (str) => { 
 //   const stack = new Stack();
 //   for (const symbol of str) {
-//     if(symbol==='('){ 
+//     if(symbol==='(' || symbol==='['){ 
 //       stack.push(symbol);
 //     }
 //     if(stack.isEmpty){ 
 //       return false;
 //     }
-//     if(symbol===')'){
+//     if(symbol===')' || symbol===']'){
 //       stack.pop();
 //     }
 //   }
@@ -44,23 +44,25 @@ const options = {
 const checkSequence = (str, options) => { 
   const brackets = options.brackets; //{'(':')','{':'}','[':']',}
   //тут надо собрать все значения объекта brackets в массив
+  const closesBrackets = Object.values(brackets); //[')','}',']']
   const stack = new Stack();
-  for (const symbol of str) {  //')'
-    if(brackets[symbol] !== undefined){ //brackets['(']=>')'
+  for (const symbol of str) {  //'2'
+    if(brackets[symbol] !== undefined){ //brackets[')']=>undefined
       stack.push(symbol);               //'('
       continue;
     }
     // if(stack.isEmpty){ 
     //   return false;
     // }
-    const lastOfStack = stack.pick(); //'('
-    const correctSymbol = brackets[lastOfStack];
+    //const lastOfStack = stack.pick(); //'('
+    const correctSymbol = brackets[stack.pick()];//')','}',']'
     if(symbol === correctSymbol){ //')'
       stack.pop();
-    }else{ //тут надо дать возможность пропускать не скобки
+    }else if(closesBrackets.includes(symbol)){ 
+      //тут надо дать возможность пропускать не скобки
       return false;
     }
   }
   return stack.isEmpty;
 }
-console.log(checkSequence(')()', options));
+console.log(checkSequence('{[(2+5)*]8}', options));
