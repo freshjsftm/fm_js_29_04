9c15aa6b07bc650935703afa16639e0bc1dbe639
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-27 11:05:05 +0300

    Add Set syntax

--- index.js
+++ index.js
@@ -25,4 +25,30 @@ const translate = (str, dict, separator = " ") => {
     .join(separator);
 };
 
-console.log(translate(str1, dictionary));
+//console.log(translate(str1, dictionary));
+
+const iterKeys = dictionary.keys();
+const arrKeysDict = [...iterKeys];
+// for (const iterator of dictionary.keys()) {
+//   arrKeysDict.push(iterator)
+// }
+
+const arrValuesDict = [...dictionary.values()];
+
+const obj7 = {7:7};
+const key = obj7; //link
+
+const mySet = new Set();
+mySet.add(7);
+mySet.add(7);
+mySet.add(7);
+mySet.add('7');
+mySet.add(obj7); //link
+// mySet.add([7]);  //link
+mySet.add({7:7}); //link
+// mySet.add([7]); //link
+mySet.add(key);
+
+console.log(mySet.has({7:7})) //false
+console.log(mySet.has(obj7)) //true
+console.log(mySet)
