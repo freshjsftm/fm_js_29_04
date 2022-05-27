258941160952c04e7d9bfe033b1c74fc3d5d5264
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-05-27 10:27:29 +0300

    Optimizate translate

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 "use strict";
 
 const dictionary = new Map();
-dictionary.set('кіт','cat');
-dictionary.set('привіт','Hello');
-dictionary.set('голова','head');
-dictionary.set('день','day');
-dictionary.set('в\'язати','to knit');
-dictionary.set('донька','daughter');
-dictionary.set('причесати','comb');
-dictionary.set('ходити','walk');
-dictionary.set('кохання','love');
-dictionary.set('поруч','near');
-dictionary.set('подобатися','like');
-dictionary.set('літо','summer');
+dictionary.set("кіт", "cat");
+dictionary.set("привіт", "Hello");
+dictionary.set("голова", "head");
+dictionary.set("день", "day");
+dictionary.set("в'язати", "to knit");
+dictionary.set("донька", "daughter");
+dictionary.set("причесати", "comb");
+dictionary.set("ходити", "walk");
+dictionary.set("кохання", "love");
+dictionary.set("поруч", "near");
+dictionary.set("подобатися", "like");
+dictionary.set("літо", "summer");
 
-const str1 = 'Донька люба мила гарна причесати кіт Літо поруч Голова подобатися поруч кохання';
+const str1 =
+  "Донька люба мила гарна причесати кіт Літо поруч Голова подобатися поруч кохання";
 
-const translate = (str, dict)=>{
-  const lowerStr = str.toLowerCase();
-  const words = lowerStr.split(' '); //донька
-  const wordsTransleted = words.map((word)=>{
-    if(dict.has(word)){
-      return dict.get(word);
-    }
-    return word;
-  })
-  return wordsTransleted.join(' ');
-}
+const translate = (str, dict, separator = " ") => {
+  return str
+    .toLowerCase()
+    .split(separator)
+    .map((word) => (dict.has(word) ? dict.get(word) : word))
+    .join(separator);
+};
 
 console.log(translate(str1, dictionary));
-
-
-
